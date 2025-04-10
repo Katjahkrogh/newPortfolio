1086a96e8cf99a1c3f2a04d4c251f0016b4ebe79
Katja Hvid Krogh <122166841+Katjahkrogh@users.noreply.github.com>
2025-04-10 22:32:25 +0200

    fix

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ import { TextAnimate } from '@/components/magicui/text-animate';
 
 const Hero = () => {
   return (
-    <section className="relative bg-gradient-to-b from-gray-100 to-gray-300 dark:from-gray-900 dark:to-black h-full w-full p-6 rounded-b-[90px] md:rounded-b-[180px] flex flex-col sm:flex-row justify-center items-center lg:justify-center gap-8 lg:gap-32 -mt-16 sm:-mt-32">
+    <section className="relative bg-gradient-to-b from-gray-100 to-gray-300 dark:from-gray-900 dark:to-black h-full w-full p-6 rounded-b-[90px] md:rounded-b-[180px] flex flex-col sm:flex-row justify-center items-center lg:justify-center gap-8 lg:gap-32 -mt-10 sm:-mt-24">
       <Image
         src="/portrait_cut.png"
         alt="Hero Image"
@@ -20,7 +20,7 @@ const Hero = () => {
           animation="blurIn"
           as="h1"
           duration={2.2}
-          className="text-4xl md:text-5xl lg:text-6xl font-medium text-primary text-pretty max-w-2xl lg:max-w-xl">
+          className="text-3xl md:text-5xl lg:text-6xl font-medium text-primary text-pretty max-w-2xl lg:max-w-xl">
           Driven by Code, Creativity, and Growth – A Developer in the Making.
         </TextAnimate>
       </div>
