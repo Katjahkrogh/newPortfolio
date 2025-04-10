69e5b9c4c9fcd16d8d00cbe0931687c9c819a977
Katja Hvid Krogh <122166841+Katjahkrogh@users.noreply.github.com>
2025-04-10 21:51:47 +0200

    added hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import Image from 'next/image';
+import { TypingAnimation } from '@/components/magicui/typing-animation';
+import { TextAnimate } from '@/components/magicui/text-animate';
 
 const Hero = () => {
   return (
-    <div>
+    <section className="relative bg-gradient-to-b from-gray-100 to-gray-300 dark:from-gray-900 dark:to-black h-full w-full p-6 rounded-b-[100px] md:rounded-b-[180px] flex flex-col sm:flex-row justify-center items-center lg:justify-center gap-8 lg:gap-32 -mt-10 sm:-mt-12">
       <Image
-        src="/portrait_cut_2.png"
+        src="/portrait_cut.png"
         alt="Hero Image"
-        width={400}
-        height={400}
+        className="h-72 w-auto sm:h-80 md:h-96 lg:h-[30rem]"
+        width={800}
+        height={800}
       />
-    </div>
+      <div className="flex flex-col justify-center items-center sm:items-start text-center sm:text-left">
+        <p className="text-lg font-medium md:text-2xl pb-1">
+          Hi, I'm Katja 👋
+        </p>
+        <TextAnimate
+          animation="blurIn"
+          as="h1"
+          duration={2.2}
+          className="text-4xl md:text-5xl lg:text-6xl font-medium text-primary text-pretty max-w-2xl lg:max-w-xl">
+          Driven by Code, Creativity, and Growth – A Developer in the Making.
+        </TextAnimate>
+      </div>
+    </section>
   );
 };
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,9 +7,9 @@ import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 const NavBar = () => {
   return (
-    <nav className="fixed top-0 left-0 w-full z-50 bg-background flex justify-between p-4 md:px-6">
+    <nav className="fixed top-0 left-0 w-full z-50 bg-transparant flex justify-between p-4 md:px-6">
       <section className="flex items-center">
-        <span className="text-xl">KK</span>
+        <span className="text-lg font-medium">KATJA KROGH.</span>
       </section>
       <section className="flex items-center justify-between pt-1 gap-2">
         <Tooltip>
